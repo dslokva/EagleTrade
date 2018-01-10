f9efe51801dd64e8098a2285f37bbd641dea99fa
Dima <Dmitriy.Slokva@gmail.com>
2018-01-10 15:39:54 +0600

    initial commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="app-footer">
+    <span><a href="http://#">Eagle Trade</a> &copy; 2018 by <a href="https://t.me/sldmk">@sldmk</a></span>
+    <span class="ml-auto">Powered by <a href="http://coreui.io">CoreUI</a></span>
+  </footer>
+</template>
+<script>
+export default {
+  name: 'footer'
+}
+</script>
